1935b178b00a1ccd0fd4fdfe9f549cdd398e87c5
Kenneth de Silva <kenndesilva1@gmail.com>
2019-07-03 15:35:52 +1000

    Added filters and layers based on subj.

--- src.r
+++ src.r
@@ -18,7 +18,7 @@ library(reshape2)
 unsw_courses.df <- read.csv("unsw-courses.csv", na.strings = "null",
                             stringsAsFactors = F)
 
-# DF of prereq links and faculty
+# DF of prereq links and faculty wrangled from raw data
 named_edges <- unsw_courses.df %>%
     na.omit() %>%
     mutate(destination = str_extract(courses, "[A-Z]{4}\\d{4}(?!\\d)")) %>%
@@ -28,17 +28,19 @@ named_edges <- unsw_courses.df %>%
     select(source = prereqs, destination, faculties)
 
 # igraph DF of nodes (id, label and faculty)
+#   Also filters by subject code or faculty
 nodes <- melt(named_edges, id.vars = 'faculties') %>%
     select(label = value, faculties) %>%
     unique() %>%
+    filter(str_detect(faculties, "Law")) %>%
     mutate(id = 1:n()) %>%
     select(id, label, faculties)
 
 # igraph DF of edges by node id
 edges <- named_edges %>%
-    left_join(nodes, by = c("source" = "label")) %>%
+    inner_join(nodes, by = c("source" = "label")) %>%
     rename(from = id) %>%
-    left_join(nodes, by = c("destination" = "label")) %>%
+    inner_join(nodes, by = c("destination" = "label")) %>%
     rename(to = id) %>%
     select(from, to)
 
@@ -46,7 +48,12 @@ edges <- named_edges %>%
 routes <- tbl_graph(nodes = nodes, edges = edges, directed = T)
 
 # Plot the routes data
-ggraph(routes, layout = "sugiyama", hgap = 4, maxiter = 10000) +
+ggraph(routes,
+       # Uses first number of subj code as layer number.
+       # layers = as.numeric(str_extract(nodes$label, "\\d{1}")),
+       layout = "sugiyama",
+       hgap = 4,
+       maxiter = 10000) +
     geom_edge_diagonal(alpha = 0.1,
                        colour = 'white') +
     geom_node_point(size = 1.5, aes(colour = faculties)) +
@@ -58,3 +65,4 @@ ggraph(routes, layout = "sugiyama", hgap = 4, maxiter = 10000) +
         legend.title = element_text(colour = "white")
     ) +
     scale_colour_brewer(palette = "Set1")
+
